34260f67be84d8a41708488414d22357993cb0ef
altoledo27 <altoledoz27@gmail.com>
2026-04-18 05:24:13 -0600

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
-import LandingPage from "./LandingPage";
-import Translator from "./Translator";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage"; // Asegúrate de que la ruta del archivo sea correcta
+import AuraSystem from "./pages/AuraSystem";   // El componente donde está la IA y la cámara
 
 export default function App() {
-  const [started, setStarted] = useState(false);
-
-  return started
-    ? <Translator />
-    : <LandingPage onStart={() => setStarted(true)} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/traductor" element={<AuraSystem />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
